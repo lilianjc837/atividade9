d0d83c1958cf1901bcdf07da0bf3b22dd0ad3851
Lilian de Jesus Correia <lilyhttpszzzz@gmail.com>
2024-05-17 19:32:41 +0000

    criação dos primeiros estilos

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { HomeContainer } from "./Home.styles";
+
+export default function Home() {
+  return (
+    <HomeContainer>
+      <h1>
+        Liliannnnn
+      </h1>
+    </HomeContainer>
+  )
+}
